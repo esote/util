e0dc86b25e4c09924f4196a26bc81367f2902185
esote <esote.net@gmail.com>
2020-01-15 00:31:29 -0600

    Backlog flushing

--- pool/pool.go
+++ pool/pool.go
@@ -1,10 +1,14 @@
 // Package pool provides a simple, versatile worker pool implementation.
 package pool
 
-import "sync"
+import (
+	"sync"
+	"sync/atomic"
+)
 
 // Pool is a worker pool.
 type Pool struct {
+	done uint64
 	jobs chan job
 	wg   sync.WaitGroup
 }
@@ -34,13 +38,14 @@ func New(workers, backlog int) *Pool {
 }
 
 // Enlist the worker pool with a new job. The arguments will be passed to f. If
-// blocking, this function waits until a worker has accepted the job. Otherwise
-// this function returns whether a worker was available to accept the job.
+// blocking, this function waits until a worker has accepted the job. This
+// function returns whether a worker was available to accept the job.
 func (p *Pool) Enlist(block bool, f func(args ...interface{}), args ...interface{}) bool {
 	if block {
 		p.jobs <- job{f, args}
 		return true
 	}
+
 	select {
 	case p.jobs <- job{f, args}:
 		return true
@@ -50,8 +55,18 @@ func (p *Pool) Enlist(block bool, f func(args ...interface{}), args ...interface
 }
 
 // Close the worker pool. Waits for the job backlog to be consumed. Further use
-// of the pool is undefined behavior.
-func (p *Pool) Close() {
+// of the pool is undefined behavior. When flushing, the jobs in the job backlog
+// are ignored and simply consumed rather than called.
+func (p *Pool) Close(flush bool) {
+	var n uint64
+	if flush {
+		n = 1
+	} else {
+		defer atomic.SwapUint64(&p.done, 1)
+	}
+	if !atomic.CompareAndSwapUint64(&p.done, 0, n) {
+		panic("pool: close on closed pool")
+	}
 	close(p.jobs)
 	p.wg.Wait()
 }
@@ -59,6 +74,9 @@ func (p *Pool) Close() {
 func (p *Pool) worker() {
 	defer p.wg.Done()
 	for job := range p.jobs {
+		if atomic.CompareAndSwapUint64(&p.done, 1, 1) {
+			continue
+		}
 		job.f(job.args...)
 	}
 }
--- pool/pool_test.go
+++ pool/pool_test.go
@@ -3,6 +3,7 @@ package pool
 import (
 	"sync/atomic"
 	"testing"
+	"time"
 )
 
 const n = 50000
@@ -18,7 +19,7 @@ func TestPool(t *testing.T) {
 		p.Enlist(true, f, i)
 	}
 
-	p.Close()
+	p.Close(false)
 
 	if i != n {
 		t.Fatalf("%d != %d", i, n)
@@ -40,9 +41,39 @@ func TestPoolNonblocking(t *testing.T) {
 		}
 	}
 
-	p.Close()
+	p.Close(false)
 
 	if i+dropped != n {
 		t.Fatalf("%d+%d != %d", i, dropped, n)
 	}
+}
+
+func TestCloseFlush(t *testing.T) {
+	var i uint64
+
+	f := func(args ...interface{}) {
+		time.Sleep(50 * time.Millisecond)
+		atomic.AddUint64(&i, 1)
+	}
+
+	p := New(1, 1)
+	p.Enlist(true, f, 0)
+	p.Enlist(true, f, 1)
+	p.Close(false)
+
+	if i != 2 {
+		t.Fatalf("expected %d got %d", 2, i)
+	}
+
+	i = 0
+	p = New(1, 1)
+	p.Enlist(true, f, 0)
+	p.Enlist(true, f, 1)
+	p.Close(true)
+
+	// The second, buffered item will now be flushed.
+	if i != 1 {
+		t.Fatalf("expected %d got %d", 1, i)
+	}
+
 }
